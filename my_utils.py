263602baebc90f3c709a2db0930c6813b81fb299
Tayler Martin <tayler.martin@colorado.edu>
2025-09-09 22:34:34 -0600

    Filled get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,12 @@
+import csv
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    f = open(file_name,'r', encoding="utf-8")
+    results = []
+    for line in f:
+        line = line.strip()
+        row = line.split(',')
+        if line[query_column] == query_value:
+            results.append(row[result_column])
+    f.close()
+    return results
